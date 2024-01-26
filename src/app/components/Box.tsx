46d17220ab3a557b96e308cd542bca2dbd494444
Evan Hickman <eh@Evans-MacBook-Pro.local>
2024-01-25 20:29:11 -0600

    add frosted overlay

--- src/app/components/Box.tsx
+++ src/app/components/Box.tsx
@@ -22,33 +22,18 @@ const Box = ({
   isActive,
   onClick,
 }: BoxProps) => {
-  console.log(isActive);
-  const variants = {
-    visible: {
-      x: '0',
-      gridRow: 'span 1',
-      transition: { duration: 0.5, delay: id * 0.1 },
-    },
-    active: {
-      x: '0',
-      gridRow: `span 50`,
-      transition: { type: 'spring', duration: 1, delay: 0, layout: true },
-    },
-  };
-
   return (
     <AnimatePresence>
       <motion.div
         className={`${classNames} flex flex-col justify-center row-span-1 cursor-pointer z-10`}
-        initial={{ x: '100%', gridRow: 'span 1' }}
-        animate={isActive ? variants.active : variants.visible}
+        initial={{ x: '100%' }}
+        animate={{ x: 0 }}
         transition={{
           type: 'spring',
           duration: 0.6,
           delay: id * 0.1,
         }}
         exit={{ x: '100%' }}
-        variants={variants}
         onClick={() => onClick(id)}
       >
         <div className='pl-40'>
