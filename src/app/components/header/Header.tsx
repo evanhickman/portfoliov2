f62bcd8abdface6411b98f43dc7aa028006881bb
Evan Hickman <travisevanhickman@gmail.com>
2026-02-18 23:11:04 -0500

    hook

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,22 +1,5 @@
 import Link from 'next/link';
-import contactContent from '@/lib/content/contact';
-import homeContent from '@/lib/content/home';
-import workContent from '@/lib/content/work';
-
-const NAV_LINKS = [
-	{
-		name: homeContent.navTitle,
-		link: homeContent.url,
-	},
-	{
-		name: workContent.navTitle,
-		link: workContent.url,
-	},
-	{
-		name: contactContent.navTitle,
-		link: contactContent.url,
-	},
-] as const;
+import { NAV_LINKS } from '@/lib/hooks/useContent';
 
 export default function Header() {
 	return (
